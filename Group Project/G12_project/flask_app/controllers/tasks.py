e97ea4ffbbd612b51875b68b88d4579c7d42761a
dalton-john7 <106289848+dalton-john7@users.noreply.github.com>
2023-01-27 23:04:12 -0500

    message

--- Group Project/G12_project/flask_app/controllers/tasks.py
+++ Group Project/G12_project/flask_app/controllers/tasks.py
@@ -19,7 +19,6 @@ def tasks_home():
 def task_detail(task_id):
     user = User.get_by_id(session["user_id"])
     task = Task.get_by_id(task_id)
-    # need a way to retrieve all comments
     return render_template("task_detail.html", user=user, task=task)
 
 
