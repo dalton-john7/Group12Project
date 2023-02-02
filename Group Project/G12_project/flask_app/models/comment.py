f4d491402c4966d7fbdb0ddc9306ec0086ef377d
Elvin Ramirez <102904220+ElvinRamirez@users.noreply.github.com>
2023-02-01 19:17:44 -0500

    updated query to include user commenting - need to work on displaying the username

--- Group Project/G12_project/flask_app/models/comment.py
+++ Group Project/G12_project/flask_app/models/comment.py
@@ -23,9 +23,9 @@ class Comment:
 
     @classmethod
     def get_all(cls, data):
-        query = "SELECT * FROM tasks LEFT JOIN comments ON comments.task_id = tasks.id WHERE comments.task_id = %(id)s;"
+        query = "SELECT * FROM tasks LEFT JOIN comments ON comments.task_id = tasks.id JOIN users ON users.id = comments.user_id WHERE comments.task_id = %(id)s;"
         results = connectToMySQL(DB).query_db(query, data)
-        print(results)
+        
         comments = []
         for comment in results:
             comment_data = cls(comment)
@@ -33,13 +33,14 @@ class Comment:
                 {   
                     'id':comment["task_id"],
                     'tip':comment["tip"],
+                    'username': comment["username"],
                     'taskname':comment["taskname"],
                     "created_at": comment["created_at"],
                     "updated_at": comment["updated_at"]
                 }
             )
+            print(results)
             comments.append(comment_data)
-            #print(comments)
         return comments
 #Commenting this method for now - will be working on the above
     # @classmethod
